0459a9c828968ac183a3b99e371bdbd632a18519
Kono <konotorie@gmail.com>
2018-10-15 10:29:09 +0900

    正解したと思ったけど後半WA

--- abc2/problemD.py
+++ abc2/problemD.py
@@ -1,9 +1,23 @@
+import itertools
 n,m = map(int, input().split())
 relation = {}
 for i in range(m):
     x, y = map(int, input().split())
-    relation.setdefault(x,[]).append(y) #javaのHash宣言みたい
-    relation.setdefault(y,[]).append(x)
-print(relation)
+    relation.setdefault(x,[x]).append(y) #javaのHash宣言みたい
+    relation.setdefault(y,[y]).append(x)
+groups = list(relation.values())
+# print(groups)
 #TODO'違いに知り合い'という状態をどうやって表すか
-#各valueの中身を見に行ってkeyが入っている数を得る?
+# 重複組み合わせを考えてそれらの最大論理積が答えでは?
+results = []
+for i in range(2,len(groups)+1):
+    for v in itertools.combinations(groups,i):
+        # print(v)
+        intersection = set(v[0])
+        for each in v:
+            intersection = intersection & set(each)
+        results.append(len(intersection))
+if results:
+    print(max(results))
+else:
+    print(1)
